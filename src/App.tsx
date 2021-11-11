5cfadac1250f4e63c3ae2b110f5fbdddba9d0c30
Leandro Camilotti <lecamilotti@gmail.com>
2021-11-11 20:54:24 +0100

    Created Modal as component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -22,13 +23,11 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-
-      <Modal 
+      <NewTransactionModal 
       isOpen={isNewTransactionModalOpen}
       onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Register new transaction</h2>
-      </Modal>
+      />
+      
      <GlobalStyle />
     </>
   );
